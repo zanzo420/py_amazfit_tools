2e61877c9a56ec5930bf89459e21f928a93c46c4
dpeddi <dpeddi@gmail.com>
2020-01-11 22:38:28 +0100

    some code cleanup

--- watchFaceParser/models/elements/weatherElement.py
+++ watchFaceParser/models/elements/weatherElement.py
@@ -38,29 +38,9 @@ class WeatherElement(ContainerElement):
             self._icon = ExtendedWeatherElement(parameter = parameter, parent = self, name = 'Icon')
             return self._icon
         elif parameterId == 2: #temperature(text)
-#            print ("WeatherElement: temperature(text) writing supported")
             from watchFaceParser.models.elements.weather.temperatureElement import TemperatureElement
             self._temperature = TemperatureElement(parameter = parameter, parent = self, name = 'Temperature')
             return self._temperature
-#        elif parameterId == 3:
-#            from watchFaceParser.models.elements.common.twoDigitsElement import TwoDigitsElement
-#            self._seconds = TwoDigitsElement(parameter = parameter, parent = self, name = 'Seconds')
-#            return self._seconds
-#        elif parameterId == 4:
-#            from watchFaceParser.models.elements.time.amPmElement import AmPmElement
-#            self._amPm = AmPmElement(parameter = parameter, parent = self, name = 'AmPm')
-#            return self._amPm
-#        elif parameterId == 5:
-#            pass
-#        elif parameterId == 10:
-#            from watchFaceParser.models.elements.common.imageElement import ImageElement
-#            self._delimiter = ImageElement(parameter = parameter, parent = self, name = 'Delimiter')
-#            return self._delimiter
-#        elif parameterId == 12:
-#            from watchFaceParser.models.elements.time.pmElement import PmElement
-#            self._pm = PmElement(parameter = parameter, parent = self, name = 'Pm')
-#            return self._pm
         else:
-#            print ("WeatherElement: unsupported", parameterId)
             return super(WeatherElement, self).createChildForParameter(parameter)
 
