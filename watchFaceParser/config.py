91f25c1014c5b9f0d98e9f4810a3283ed5e6767a
dpeddi <dpeddi@gmail.com>
2020-01-16 15:14:40 +0100

    autodetect device

--- watchFaceParser/config.py
+++ watchFaceParser/config.py
@@ -3,16 +3,41 @@ class Config:
     _is_gts = False
     _image_size = (360, 360)
     _preview_size = (210, 210)
+    _autodetect = True
 
+    _size_gtr_47 = (454, 454)
+    _size_gtr_42 = (390, 390)
+    _size_gts = (348,442)
+
+    @staticmethod
+    def getAutodetect():
+	    return Config._autodetect
+	
+    @staticmethod
+    def autodetect(size):
+        print ("Autodetect device")
+        if size == Config._size_gtr_47:
+            print("Detected GTR (47mm)")
+            Config.setGtrMode(47)
+        elif size == Config._size_gtr_42:
+            print("Detected GTR (42mm)")
+            Config.setGtrMode(42)
+        elif size == Config._size_gts:
+            print("Detected GTS")
+            Config.setGtsMode(True)
+        else:
+            print("default_verge")
 
     @staticmethod
     def setGtrMode(gtr):
         Config._is_gtr = gtr
         if Config._is_gtr == 47:
-            Config._image_size = (454, 454)
+            Config._autodetect = False
+            Config._image_size = Config._size_gtr_47
             Config._preview_size = (266, 266)
         if Config._is_gtr == 42:
-            Config._image_size = (390, 390)
+            Config._autodetect = False
+            Config._image_size = Config._size_gtr_42
             Config._preview_size = (266, 266)
 
     @staticmethod
@@ -22,8 +47,9 @@ class Config:
     @staticmethod
     def setGtsMode(gts):
         if gts:
+            Config._autodetect = False
             Config._is_gts = 40
-            Config._image_size = (348,442)
+            Config._image_size = Config._size_gts
             Config._preview_size = (242,304)
 
     @staticmethod
